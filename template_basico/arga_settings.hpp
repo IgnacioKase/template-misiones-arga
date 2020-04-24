ff9bca7f5611eef4ec069a6785a04a80463a2696
FrancoCarlino <62719357+FrancoCarlino@users.noreply.github.com>
2020-04-24 15:51:08 -0300

    Update arga_settings.hpp

--- template_basico/arga_settings.hpp
+++ template_basico/arga_settings.hpp
@@ -40,7 +40,7 @@ ENEMIGOS = ["", ""];
 
 //IA - CONFIGURACION
 DESACTIVAR_IA_DE_GRUPO = 1; // 0 Permite la IA del Grupo de los jugadores, 1 la desactiva.
-DESACTIVAR_TODO_BLUFOR = 1; // 0 Permite la IA del Blufor, 1 la desactiva.
+DESACTIVAR_TODO_BLUFOR = 0; // 0 Permite la IA del Blufor, 1 la desactiva.
 DIFICULTAD_IA_PERSONALIZADA = 0;
 AIMING_ACCURACY = 0.01; //precision de apuntado
 AIMING_SHAKE = 0.99; 	//dispersión de apuntado
